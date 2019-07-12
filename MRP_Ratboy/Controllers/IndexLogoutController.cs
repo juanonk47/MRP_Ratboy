9092576876826f7e72e4c4a1034b0950437c7d0f
Eddjoy <diegolalo.1997@gmail.com>
2019-07-12 12:54:47 -0500

    Integración de Layout en mayoria de las vistas

---
+++ MRP_Ratboy/Controllers/IndexLogoutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MRP_Ratboy.Controllers
+{
+    public class IndexLogoutController : Controller
+    {
+        // GET: IndexLogout
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
