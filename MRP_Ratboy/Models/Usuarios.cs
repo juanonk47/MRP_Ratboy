798b6132f0da9ca5547d02d09304132a3a7adfe8
Juan Octavio <>
2019-07-29 22:41:10 -0500

    Integracion compatibilidad primeros metodos

--- MRP_Ratboy/Models/Usuarios.cs
+++ MRP_Ratboy/Models/Usuarios.cs
@@ -28,6 +28,7 @@ namespace MRP_Ratboy.Models
         public int estatus { get; set; }
         public int idPersona_FK { get; set; }
         public string correo { get; set; }
+
         public bool checkestatus { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
